7fc845ecaad067e72a0ef15d30d6c95f0ca5c5ff
isaac <beatbit01@gmail.com>
2022-04-26 02:29:13 -0500

    log in page ready for test

---
+++ pages/forgot-passoword.tsx
@@ -0,0 +1,9 @@
+function ForgotPasswordUpPage() {
+  return (
+    <>
+      <h1>forgot password page</h1>
+    </>
+  );
+}
+
+export default ForgotPasswordUpPage;
---
+++ pages/signup.tsx
@@ -0,0 +1,9 @@
+function SignUpPage() {
+  return (
+    <>
+      <h1>sign up page</h1>
+    </>
+  );
+}
+
+export default SignUpPage;
