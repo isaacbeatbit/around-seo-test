54b8da7f8f2be0599d083f33ae5042e62cce3fa7
isaac <beatbit01@gmail.com>
2022-04-26 02:50:28 -0500

    login page with open graph meta tags

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,23 +7,22 @@ function LogingPage() {
         <title>Around | Inicia sesión</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta property="og:title" content="Around | Inicia sesión" />
+        <meta
+          property="og:description"
+          content="Espacio de trabajo con términos y flexibles y soluciones híbridas si tu empresa necesita expandirse globalmente o necesita espacio de oficina cerca de ti. Para todo tus formas de trabajo, estamos aqui."
+        />
+        <meta property="og:image" content="/office.avif" />
+        <meta property="og:url" content="https://around.to" />
+        <meta property="og:locale" content="es_ES" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/public/office.avif" />
-        <meta property="og:url" content="https://around.to/login" />
-        <meta
-          property="og:url"
-          content="https://www.imdb.com/title/tt0117500/"
-        />
-        <meta
-          property="og:image"
-          content="https://ia.media-imdb.com/images/rock.jpg"
-        />
+        <meta property="og:site_name" content="Around" />
       </Head>
       <main
         style={{
           height: "100vh",
           display: "flex",
           alignItems: "center",
+          justifyContent: "center",
         }}
       >
         <h1>LOGIN</h1>
