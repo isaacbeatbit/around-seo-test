e3b7128a1d8157bb2118a2cc34456291ea35d861
isaac <beatbit01@gmail.com>
2022-04-27 02:10:03 -0500

    twitter card fixed #3

--- pages/summary-large-image.tsx
+++ pages/summary-large-image.tsx
@@ -8,10 +8,7 @@ function SummaryLargeImage() {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@around_app" />
         <meta name="twitter:creator" content="@ar_creator_1" />
-        <meta
-          name="twitter:image"
-          content="office.gif"
-        />
+        <meta name="twitter:image" content="/office.jpg" />
       </Head>
       <main>
         <h1>summary large image</h1>
