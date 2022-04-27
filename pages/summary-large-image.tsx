60fbbc96aa5488ea4d19abaa3ee9a7affc0acd60
isaac <beatbit01@gmail.com>
2022-04-27 02:14:51 -0500

    twitter card fixed #5

--- pages/summary-large-image.tsx
+++ pages/summary-large-image.tsx
@@ -4,7 +4,7 @@ function SummaryLargeImage() {
   return (
     <>
       <Head>
-        <title>Around | Summary with large image</title>
+        {/* <title>Around | Summary with large image</title> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@around_app" />
         <meta name="twitter:creator" content="@ar_creator_1" />
